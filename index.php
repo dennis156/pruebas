4d6dc82b4739cfc2d02f6bfd74e1a76103085776
setoxxD <115164130+dennis156@users.noreply.github.com>
2022-11-24 09:50:52 -0600

    pruebas personales

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<!--  -->
+
+<?php  
+  require_once  "./views/header.php";
+
+?>
+<body>
+ <?php
+   require_once "./views/navbar.php";
+   require_once "./views/home.php";
+  require_once   "./scripts/bootstrap-js.php";
+ ?>
+
+
+</body>
+</html>
